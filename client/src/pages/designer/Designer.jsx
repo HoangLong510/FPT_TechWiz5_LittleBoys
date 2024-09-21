10f83aff122f43e660895d584bf15be0224c7036
nhlinh0302 <nhatlinh3b122@gmail.com>
2024-09-22 01:18:25 +0700

    linh-fix

--- client/src/pages/designer/Designer.jsx
+++ client/src/pages/designer/Designer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function DesignerProjectDetails() {
+export default function Designer() {
   return <div></div>;
 }
---
+++ client/src/pages/designer/project/DesignerProjectList.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DesignerProjectList() {
+  return <div></div>;
+}
